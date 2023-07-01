88467b98cae62989058289d187eed3abf7bd89db
Anantajit Raja <90404582+AnantR777@users.noreply.github.com>
2023-07-01 20:02:40 +0100

    Delete Easy directory

--- Easy/26RemoveDuplicatesfromSortedArray.py
+++
@@ -1,8 +0,0 @@
-class Solution:
-    def removeDuplicates(self, nums: List[int]) -> int:
-        unique_nums = []
-        for num in nums:
-            if num not in unique_nums:
-                unique_nums.append(num)
-        nums[:] = unique_nums  # Update the original list
-        return len(nums)
